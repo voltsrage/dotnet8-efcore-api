67ff15f990bb77366fc0f6375afad0c5cf76d2f6
voltsrage <electric5388@hotmail.com>
2025-03-14 17:15:41 +0800

    First Add Entities

---
+++ EFCore/EFCore.API/Entities/BaseModel/BaseEntity.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCore.API.Entities.BaseModel
+{
+    /// <summary>
+    /// Implement base entity
+    /// </summary>
+    public class BaseEntity : IEntity
+    {
+        /// <summary>
+        /// Id of entity
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Guid of entity
+        /// </summary>
+        [StringLength(36)]
+        public string Guid { get; set; } = System.Guid.NewGuid().ToString();
+
+        /// <summary>
+        /// Displays the status of the Entity e.g. deleted, active e.g. 
+        /// </summary>
+        public int EntityStatusId { get; set; } = 1;
+
+        /// <summary>
+        /// CreateAt    
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// When this record is updated
+        /// </summary>
+        public DateTime? UpdatedAt { get; set; }
+
+        /// <summary>
+        /// who create this record
+        /// </summary>
+        public int? CreatedBy { get; set; }
+
+        /// <summary>
+        /// who update this record
+        /// </summary>
+        public int? UpdatedBy { get; set; }
+    }
+}
---
+++ EFCore/EFCore.API/Entities/BaseModel/IEntity.cs
@@ -0,0 +1,54 @@
+﻿
+namespace EFCore.API.Entities.BaseModel
+{
+    /// <summary>
+    /// Base entity interface
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// Create time timestamp
+        /// </summary>
+        long CreateAt { get; set; }
+
+        /// <summary>
+        /// Creator of record
+        /// </summary>
+        int? CreateBy { get; set; }
+
+        /// <summary>
+        /// Id of entity
+        /// </summary>
+        int Id { get; set; }
+
+        /// <summary>
+        /// Guid of entity
+        /// </summary>
+        string Guid { get; set; }
+
+        /// <summary>
+        /// Displays the status of the Entity e.g. deleted, active e.g. 
+        /// </summary>
+        public int EntityStatusId { get; set; }
+
+        /// <summary>
+        /// CreateAt    
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// When this record is updated
+        /// </summary>
+        public DateTime? UpdatedAt { get; set; }
+
+        /// <summary>
+        /// who create this record
+        /// </summary>
+        public int? CreatedBy { get; set; }
+
+        /// <summary>
+        /// who update this record
+        /// </summary>
+        public int? UpdatedBy { get; set; }
+    }
+}
