ae82efd07de5104eac31d71b83dd4b6392362794
voltsrage <electric5388@hotmail.com>
2025-03-14 19:10:27 +0800

    Add Validators and HelperFunctions (currently only validation helper)

---
+++ EFCore/EFCore.API/Helpers/HelperFunctions.cs
@@ -0,0 +1,42 @@
+﻿using EFCore.API.Models;
+using FluentValidation;
+
+namespace EFCore.API.Helpers
+{
+    public class HelperFunctions : IHelperFunctions
+    {
+        /// <inheritdoc />/>
+        public async Task<Response<V>> ProcessValidation<T, V>(AbstractValidator<T> validator, T obj, Response<V> result)
+        {
+            var validationResult = await validator.ValidateAsync(obj);
+
+            if (!validationResult.IsValid)
+            {
+                foreach (var error in validationResult.Errors)
+                {
+                    result.ValidationErrors.Add(new ValidationError
+                    {
+                        Property = error.PropertyName,
+                        Error = error.ErrorMessage,
+                        AttemptedValue = error.AttemptedValue
+                    });
+                }
+
+                var errorToReturn = validationResult.Errors.FirstOrDefault();
+                result.IsSuccess = false;
+
+                if (errorToReturn != null)
+                {
+                    result.ErrorMessage = $"{errorToReturn.PropertyName}: {errorToReturn.ErrorMessage}";
+                }
+
+            }
+            else
+            {
+                result.IsSuccess = true;
+            }
+
+            return result;
+        }
+    }
+}
